f614ebe58a8c4badf503ad6cd8d78b00cfd98752
Nicolas Berthou <berthou.nicolas@laposte.net>
2023-06-08 15:00:26 +0200

    ADDED global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { createGlobalStyle } from 'styled-components'
 
 import type { AppProps } from 'next/app'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
